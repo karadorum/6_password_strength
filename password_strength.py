1020ae5ef15c74e4edbf7ccf42864c8f8a5bcfc9
Oleg Blinov <contextstudio78@gmail.com>
2018-09-13 21:33:20 +0300

    Update password_strength.py

--- password_strength.py
+++ password_strength.py
@@ -1,8 +1,30 @@
 
-
 def get_password_strength(password):
-    pass
+    symbols = '~`!@#$%^&*()_-+={}[]:>;\',</?"/\\'
+    num = len([int(i) for i in password if i.isdigit()])
+    up = len([str(i) for i in password if i.isupper()])
+    lower = len([str(i) for i in password if i.islower()])
+    symbol = len([str(i) for i in password if i in symbols])
+    password_lenght = len(password)
+    num_list = [num, up, lower, symbol]
+    return num_list
 
+def password_rating(num_list, password_lenght):
+    rating = 0
+    for n in num_list:
+        if n == 1:
+            rating += 1
+        elif n >= 2:
+            rating += 2
+    
+    if password_lenght >= 10:
+        rating += 2
+    return rating
+        
+    
 
 if __name__ == '__main__':
-    pass
+    password = input('type your password: ')
+    rating = password_rating(get_password_strength(password), len(password))
+    print('your password rating is {}'.format(rating))
+    
