2e179a768928a2da3ef42951053fdc0daaa92c91
Oleg Blinov <contextstudio78@gmail.com>
2018-09-17 22:14:53 +0300

    Changed names of variables

--- password_strength.py
+++ password_strength.py
@@ -1,29 +1,33 @@
+from getpass import getpass
+import string
 
-def get_password_strength(password):
-    symbols = '~`!@#$%^&*()_-+={}[]:>;\',</?"/\\'
-    number = len([int(char) for char in password if char.isdigit()])
+
+def get_password_symbols(password):
+    '''symbols = '~`!@#$%^&*()_-+={}[]:>;\',</?"/\\'''
+    number = len([char for char in password if char.isdigit()])
     upper = len([str(char) for char in password if char.isupper()])
     lower = len([str(char) for char in password if char.islower()])
-    symbol = len([str(char) for char in password if char in symbols])
+    punctuation = len([str(char) for char in password if char in string.punctuation])
     password_lenght = len(password)
-    quantity_list = [number, upper, lower, symbol]
-    return num_list
+    symb_quantity_list = [number, upper, lower, punctuation]
+    return symb_quantity_list
 
 
-def password_rating(quantity_list, password_lenght):
+def get_password_rating(symb_quantity_list, password_lenght):
     rating = 0
-    for quantity in num_list:
+    medium_lenght_password = 10
+    for quantity in symb_quantity_list:
         if quantity == 1:
             rating += 1
         elif quantity >= 2:
             rating += 2
 
-    if password_lenght >= 10:
+    if password_lenght >= medium_lenght_password:
         rating += 2
     return rating
 
 
 if __name__ == '__main__':
-    password = input('type your password: ')
-    rating = password_rating(get_password_strength(password), len(password))
+    password = getpass(prompt='type your password: ')
+    rating = get_password_rating(get_password_symbols(password), len(password))
     print('your password rating is {}'.format(rating))
